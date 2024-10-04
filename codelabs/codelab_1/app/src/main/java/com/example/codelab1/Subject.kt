c124ff9aac89405a2a331788bca3e225bec3e226
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-10-04 20:44:24 +0100

    modified implementation

--- codelabs/codelab_1/app/src/main/java/com/example/codelab1/Subject.kt
+++ codelabs/codelab_1/app/src/main/java/com/example/codelab1/Subject.kt
@@ -3,7 +3,7 @@ package com.example.codelab1
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class Topic(
+data class Subject(
     @StringRes val name: Int,
     val availableCourses: Int,
     @DrawableRes val imageRes: Int
---
+++ codelabs/codelab_1/app/src/main/java/com/example/codelab1/SubjectsData.kt
@@ -0,0 +1,34 @@
+package com.example.codelab1
+
+object SubjectsData {
+    val subjects = listOf(
+        Subject(R.string.architecture, 58, R.drawable.architecture),
+        Subject(R.string.crafts, 121, R.drawable.crafts),
+        Subject(R.string.business, 78, R.drawable.business),
+        Subject(R.string.culinary, 118, R.drawable.culinary),
+        Subject(R.string.design, 423, R.drawable.design),
+        Subject(R.string.fashion, 92, R.drawable.fashion),
+        Subject(R.string.film, 165, R.drawable.film),
+        Subject(R.string.gaming, 164, R.drawable.gaming),
+        Subject(R.string.drawing, 326, R.drawable.drawing),
+        Subject(R.string.lifestyle, 305, R.drawable.lifestyle),
+        Subject(R.string.music, 212, R.drawable.music),
+        Subject(R.string.painting, 172, R.drawable.painting),
+        Subject(R.string.photography, 321, R.drawable.photography),
+        Subject(R.string.tech, 118, R.drawable.tech),
+        Subject(R.string.architecture, 58, R.drawable.architecture),
+        Subject(R.string.crafts, 121, R.drawable.crafts),
+        Subject(R.string.business, 78, R.drawable.business),
+        Subject(R.string.culinary, 118, R.drawable.culinary),
+        Subject(R.string.design, 423, R.drawable.design),
+        Subject(R.string.fashion, 92, R.drawable.fashion),
+        Subject(R.string.film, 165, R.drawable.film),
+        Subject(R.string.gaming, 164, R.drawable.gaming),
+        Subject(R.string.drawing, 326, R.drawable.drawing),
+        Subject(R.string.lifestyle, 305, R.drawable.lifestyle),
+        Subject(R.string.music, 212, R.drawable.music),
+        Subject(R.string.painting, 172, R.drawable.painting),
+        Subject(R.string.photography, 321, R.drawable.photography),
+        Subject(R.string.tech, 118, R.drawable.tech)
+    )
+}
